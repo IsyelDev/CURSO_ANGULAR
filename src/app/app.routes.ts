42ffa7dcf18f010341902e65fcc2659d50bbb685
elmer de la paz <emdelapazt@gmail.com>
2026-02-13 18:31:33 -0500

    añadiendo rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { EmployesList } from './componentes/employes/employes-list/employes-list
 import { CompanyList } from './componentes/company/company-list/company-list';
 
 export const routes: Routes = [
-{path:"employees", component:EmployesList},
-{path:"company", component:CompanyList},
-{path:"employesDetails", component:EmployesDetails},
-{path:'',redirectTo:"employees",pathMatch:"full"}
+{path:"empleado", component:EmployesList},
+{path:"compania", component:CompanyList},
+{path:"empleadoDetalle", component:EmployesDetails},
+{path:'',redirectTo:"empleado",pathMatch:"full"}
 ];

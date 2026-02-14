34b5b6d65dd06495542d43a153771f9c29122bef
elmer de la paz <emdelapazt@gmail.com>
2026-02-13 19:08:39 -0500

    Creando tu pagina de not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
-import { EmployesDetails } from './../../../app-demo-standalone-v2/src/app/components/employes/employes-details/employes-details';
 import { Routes } from '@angular/router';
 import { EmployesList } from './componentes/employes/employes-list/employes-list';
 import { CompanyList } from './componentes/company/company-list/company-list';
+import { EmployesDetails } from './componentes/employes/employes-details/employes-details';
+import { PageNotFound } from './componentes/errores/page-not-found/page-not-found';
 
 export const routes: Routes = [
 {path:"empleado", component:EmployesList},
 {path:"compania", component:CompanyList},
 {path:"empleadoDetalle", component:EmployesDetails},
-{path:'',redirectTo:"empleado",pathMatch:"full"}
+{path:"404", component:PageNotFound},
+{path:'',redirectTo:"/empleado",pathMatch:"full"},
+{path:'**',redirectTo:"/404",pathMatch:"full"}
 ];
